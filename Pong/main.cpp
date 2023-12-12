8ee3d34d8650c51361e0b999684ddee97d5acbe5
julien_ssm <89165787+IjulienI@users.noreply.github.com>
2023-12-12 21:20:56 +0100

    [refactor] base

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,11 +1,39 @@
 #include <raylib.h>
+const int LARGEUR_ECRAN = 800;
+const int HAUTEUR_ECRAN = 450;
+
+void load();
+void update();
+void draw();
+void unload();
 
 int main() {
-	InitWindow(1080, 720, "NoEngine");
-
+	load();
 	while (!WindowShouldClose()) {
+		update();
+		draw();
 	}
-
-	CloseWindow();
+	unload();
 	return 0;
+}
+
+void load()
+{
+	InitWindow(LARGEUR_ECRAN,HAUTEUR_ECRAN,"NoEngine");
+	SetTargetFPS(60);
+}
+
+void update()
+{
+	
+}
+
+void draw()
+{
+	
+}
+
+void unload()
+{
+	
 }
